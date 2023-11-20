a6e315650d16a0516a3885b0f67aae3843c24ccd
unknown <rifkymuhamad0022@gmail.com>
2023-11-20 16:09:19 +0700

    replace and updated file

--- vitejs/src/App.jsx
+++ vitejs/src/App.jsx
@@ -1,33 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Typewriter from "typewriter-effect";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  // eslint-disable-next-line no-unused-vars
+  const [titles, setTitles] = useState(["Rifky Muhamad..", "Dyone-Strankers.", "ディオン・ストランカース", "디온 수토랑쿠","Wanna be Generalist.", "Wanna be Expert.", "Love Language!", "Full Time Learner!", "*(3rNd&#0R3IM(_)@$"]);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Typewriter
+                options={{
+                  autoStart: true,
+                  loop: true,
+                  delay: 40,
+                  strings: titles,
+                }}
+              />
     </>
   )
 }
