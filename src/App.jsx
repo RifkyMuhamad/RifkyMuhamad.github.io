2e4064f28ed18a724a5e3979b24f9542c06f5dc3
unknown <rifkymuhamad0022@gmail.com>
2023-12-23 00:59:35 +0700

    add connection to rest api

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import axios from "axios";
 import './App.css'
+import { useEffect } from "react";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const apiUrl = "https://dyonestrankers-nodejs-api.vercel.app/";
+        const response = await axios.get(apiUrl);
+        const messageValue = response.data.message;
+        setData(messageValue);
+      } catch (error) {
+        setError(error);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
-    <>
-      <div>
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-          <img src={reactLogo} className="logo react" alt="React logo" />
-      </div>
-      <h1>DYONE STRANKERS</h1>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {data ? (
+          <div>
+            <pre>{data}</pre>
+          </div>
+      ) : (
+        <p> Loading...</p>
+      )}
+      {error && <p>Error: {error.message}</p>}
+    </div>
   )
 }
 
